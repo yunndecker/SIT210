f158370d563d3a7a157124c36a3cefa2e3194616
yunndecker <84118176+yunndecker@users.noreply.github.com>
2026-05-14 15:14:49 +1000

    Update main.py

--- Task5.1GUI/main.py
+++ Task5.1GUI/main.py
@@ -14,17 +14,16 @@ def setup_leds():
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, False)
 
-def turn_all_off():
-    for pin in LED_PINS.values():
-        GPIO.output(pin, False)
-
-def select_room():
-    selected_room = room_var.get()
-    turn_all_off()
-    GPIO.output(LED_PINS[selected_room], True)
+def update_lights():
+    for room_name, pin in LED_PINS.items():
+        if room_vars[room_name].get() ==1:
+            GPIO.output(pin, True)
+        else:
+            GPIO.output(pin,False)
 
 def exit_app():
-    turn_all_off()
+    for pin in LED_PINS.values():
+        GPIO.output(pin, False)
     GPIO.cleanup()
     root.destroy()
 
@@ -32,14 +31,16 @@ root = tk.Tk()
 root.title("House Lights")
 root.geometry("250x250")
 
-room_var = tk.StringVar()
+room_vars = {}
 
 setup_leds()
 
-tk.Label(root, text="Select a Room", font=("Helvetica", 14, 'bold'), bg ="lightblue").pack(pady=10)
+tk.Label(root, text="Select a Room", font=("Helvetica", 14, "bold"), bg ="lightblue").pack(pady=10)
 
 for room_name in LED_PINS:
-    tk.Radiobutton(root,text=room_name,variable=room_var,value=room_name, command=select_room).pack(anchor="w", padx=20)
+    room_vars[room_name] = tk.IntVar()
+    
+    tk.Checkbutton(root,text=room_name,variable=room_vars[room_name],command=update_lights).pack(anchor="w", padx=20)
 
 tk.Button(root, text="Exit", command=exit_app).pack(pady=15)
 
